1e780204ad626d30d5d30bf39fdaad136327732b
Noval <mnovalhidayat298@gmail.com>
2022-03-10 15:19:22 +0700

    contoh contoh Underscore dalam Literal di kotlin

---
+++ pertemuan_1/src/main/kotlin/UnderscoreDalamLiteral.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    /**
+     * Selain memiliki literal konstan Kotlin juga dapat menggunakan underscore dalam literal, untuk
+    memberikan kemudahan pada pengembang dalam membaca dan menulis kode program.
+     */
+    val oneMillion = 1_000_000
+    val creditCardNumber = 1234_5678_9012_3456L
+    val socialSecurityNumber = 999_99_9999L
+    val hexBytes = 0xFF_EC_DE_5E
+    val bytes = 0b11010010_01101001_10010100_10010010
+    println("oneMillion = " + oneMillion)
+    println("creditCardNumber = " + creditCardNumber)
+    println("socialSecurityNumber = " + socialSecurityNumber)
+    println("hexBytes = " + hexBytes)
+    println("bytes = " + bytes)
+}
