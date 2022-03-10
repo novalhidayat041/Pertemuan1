752b67df21153c2919c91118360b5d3622e912ca
Noval <mnovalhidayat298@gmail.com>
2022-03-10 14:38:41 +0700

    pengunaan whitespace pada kotlin

--- pertemuan_1/src/main/kotlin/c.whitespace.kt
+++ pertemuan_1/src/main/kotlin/c.whitespace.kt
@@ -1,8 +1,9 @@
 fun main(args: Array<String>) {
     println("Hello, world!")
+
+    /**
+     * Seperti Java, Kotlin juga merupakan bahasa pemrograman yang ditokenisasi. Whitespace tidak
+    signifikan mempengaruhi program dan dengan aman dapat diacuhkan
+     */
 }
 
-/**
- * Seperti Java, Kotlin juga merupakan bahasa pemrograman yang ditokenisasi. Whitespace tidak
-signifikan mempengaruhi program dan dengan aman dapat diacuhkan
- */
