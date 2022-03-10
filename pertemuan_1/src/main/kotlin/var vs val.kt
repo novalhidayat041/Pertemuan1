22ff4ea164731ebb74c988e8cf747010c2927754
Noval <mnovalhidayat298@gmail.com>
2022-03-10 14:57:29 +0700

    contoh contoh variabel di kotlin

---
+++ pertemuan_1/src/main/kotlin/var vs val.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+    var varVariabel: Int
+    varVariabel = 56 //assigning first value to varVariabel
+    println("Pemberian nilai pertama pada varVariabel = "+varVariabel)
+    varVariabel = 78 //assigning second value to varVariabel
+    println("Pemberian nilai kedua pada varVariabel = "+varVariabel)
+    val phi = 3.14
+    phi = 2.5
+
+    /**
+     * Kata kunci var di Kotlin digunakan untuk mendeklarasikan variabel yang nilainya dapat berubah
+    sepanjang program dieksekusi. Sedangkan kata kunci val digunakan untuk mendeklarasikan
+    variabel konstan yang nilainya tidak akan berubah selama program dieksekusi.
+     * Ketika kita merubah nilai variabel val phi untuk kedua kalinya maka akan terdapat pesan
+    kesalahan agar kita menggunakan var variabel.
+     */
+}
