8dce61a8e28114338b04635f352adb2fdf9f30b8
Noval <mnovalhidayat298@gmail.com>
2022-03-10 14:17:57 +0700

    contoh contoh literal di kotlin

---
+++ pertemuan_1/src/main/kotlin/b.Literal.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var intLiteral = 5
+    var doubleLiteral = .02
+    var stringliteral = "Hello"
+    var charLiteral = '1'
+    var boolLiteral = true
+
+    println("intLiteral = $intLiteral")
+    println("doubleLiteral = $doubleLiteral")
+    println("stringLiteral = $stringliteral")
+    println("charLiteral = $charLiteral")
+    println("boolLiteral = $boolLiteral")
+}
