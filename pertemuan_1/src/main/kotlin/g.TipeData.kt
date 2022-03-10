e22f46584cf627d23ad3c3c358c793af2269b8c1
Noval <mnovalhidayat298@gmail.com>
2022-03-10 15:08:55 +0700

    contoh contoh Tipe Data di kotlin

---
+++ pertemuan_1/src/main/kotlin/g.TipeData.kt
@@ -0,0 +1,37 @@
+fun main(args: Array<String>) {
+    /**
+     * Tipe data di Kotlin terdiri dari Byte, Short, Int, dan Long untuk bilangan bulat (Number), Double
+    dan Float untuk bilangan desimal atau real (Floating-point), Char untuk karakter, Boolean untuk
+    kondisional (true/false), String untuk array karakter, Array yang merupakan koleksi data dengan
+    tipe data yang homogen, Collection dan Range.
+     * dibawah ini merupakan Contoh kode program menggunakan tipe data bilangan bulat
+     */
+    val minByte: Byte = -128
+    val maxByte: Byte = 127 // 8bit
+    val minShort: Short = -32768
+    val maxShort: Short = 32767 // 16bit
+    val minInt: Int = -2147483648
+    val maxInt: Int = 2147483647 // 32bit
+    val minLong: Long = -9223372036854775807
+    val maxLong: Long = 9223372036854775807 // 64bit
+    println("minByte:" + minByte)
+    println("maxByte:" + maxByte)
+    println("minShort:" + minShort)
+    println("maxShort:" + maxShort)
+    println("minInt:" + minInt)
+    println("maxInt:" + maxInt)
+    println("minLong:" + minLong)
+    println("maxLong:" + maxLong)
+    println("")
+
+    /**
+     * Contoh kode program menggunakan tipe data bilangan decimal (floating-point):
+     */
+
+
+    val maxFloat: Float = 9.123456789f
+    val maxDouble: Double = 9.123456789
+    println("maxFloat:" + maxFloat)
+    println("maxDouble:" + maxDouble)
+
+}
