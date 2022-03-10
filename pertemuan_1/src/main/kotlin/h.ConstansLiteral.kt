d50308cd9d3975b7b936135329048a5e922820cb
Noval <mnovalhidayat298@gmail.com>
2022-03-10 15:16:44 +0700

    contoh contoh Constanta Literal di kotlin

---
+++ pertemuan_1/src/main/kotlin/h.ConstansLiteral.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    val floatLiteral : Float = 178.95F //Literal Float
+    println("Contoh Literal Float : " + floatLiteral)
+    val oneBillion: Long = 1000000000L //Literal Long
+    println("Contoh Literal Long : " + oneBillion)
+    val valHeksa = 0x0F //Literal heksadesimal diawali dengan 0x
+    println("Contoh Literal Heksadesimal : " + valHeksa)
+    val valBinary = 0b00001011 //Literal binary diawali dengan 0b
+    println("Contoh Literal Binary : " + valBinary)
+    val notasiKonvensional = 123.5e10
+    println("Contoh Literal Konvensional Floating Point Number : " + notasiKonvensional)
+
+    /**
+     * Konstanta literal, atau hanya literal, adalah nilai, seperti angka, karakter, atau string yang dapat diberikan ke variabel atau konstanta simbolis, digunakan sebagai operan dalam operasi aritmatika atau logika, atau sebagai parameter untuk sebuah fungsi.
+     */
+}
