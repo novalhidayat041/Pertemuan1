bed2360784c711dddafd2f8647394d7bf242dd6d
Noval <mnovalhidayat298@gmail.com>
2022-03-10 14:30:34 +0700

    contoh contoh literal di kotlin

--- pertemuan_1/src/main/kotlin/b.Literal.kt
+++ pertemuan_1/src/main/kotlin/b.Literal.kt
@@ -10,4 +10,10 @@ fun main() {
     println("stringLiteral = $stringliteral")
     println("charLiteral = $charLiteral")
     println("boolLiteral = $boolLiteral")
+
+    /**
+     * Kotlin menyediakan beberapa literal untuk tipe dasar (Numbers, Character, Boolean, String).
+    Literal di Kotlin akan memberikan tipe data secara otomatis setelah program dikompilasi sesuai
+    dengan nilai yang cocok untuk diberikan pada suatu variabel
+     */
 }
