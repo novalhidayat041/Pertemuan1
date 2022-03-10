f4fcb96aa5c58f282d8c351adc911ab1e9043fed
Noval <mnovalhidayat298@gmail.com>
2022-03-10 14:41:01 +0700

    pengunaan whitespace pada kotlin

---
+++ pertemuan_1/src/main/kotlin/blok dan komentar.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    println("Hello, Ini Program Pertama Saya di Kotlin!")
+    //Contoh komentar satu baris
+    /*
+    Contoh komentar multi baris
+    */
+    /**
+     * Kotlin Doc --> baris ini untuk mendokumentasikan kode program
+     */
+
+    /**
+     * Blok digunakan untuk menulis ikatan sebuah statement yang dikelompokkan bersama menjadi
+    satu bagian
+    Comment adalah baris komentar pada program yang tidak akan dieksekusi oleh compiler
+     */
+}
