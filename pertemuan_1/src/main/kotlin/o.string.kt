3505f8c50a98abcf7dd123ee4873a584b3e04d8f
Noval <mnovalhidayat298@gmail.com>
2022-03-16 22:28:08 +0700

    contoh contoh character (chart) eksplisit di kotlin

---
+++ pertemuan_1/src/main/kotlin/o.string.kt
@@ -0,0 +1,15 @@
+//
+/**
+ * String (Array of Character)
+ * String merupakan array dari karakter dan bersifat immutable (bersifat statis). Elemen suatu
+ * String dapat diakses dengan operasi indeks array contoh s[i] atau dengan menggunakan for-loop.
+ * Contoh kode program mengakses elemen String:
+ */
+
+fun main(args: Array<String>) {
+    val myName: String = "M. Noval Hidayat"
+    for(chr in myName){
+        print(chr)
+    }
+    print('\n')
+}
