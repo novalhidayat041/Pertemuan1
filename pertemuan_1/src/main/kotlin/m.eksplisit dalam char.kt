68ca2062b563105b89f5abe583748e2ba776eefa
Noval <mnovalhidayat298@gmail.com>
2022-03-16 22:56:55 +0700

    contoh peksplisit dalam char

--- pertemuan_1/src/main/kotlin/m.eksplisit dalam char.kt
+++ pertemuan_1/src/main/kotlin/m.eksplisit dalam char.kt
@@ -8,7 +8,7 @@ fun main(args: Array<String>) {
     //ASCII value
     var c: Char // character
     var i: Int // ordinal (ASCII) value of the character
-    // conversion from text to ASCII value
+    // conversion from text to ASCII value.
     c = 'a'
     i = c.toInt()
     println("The character $c was converted to its ASCII value of $i")
