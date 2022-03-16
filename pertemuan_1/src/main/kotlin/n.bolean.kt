33055cfaa7ac036a947a3c9ba5236d35aab31566
Noval <mnovalhidayat298@gmail.com>
2022-03-16 22:57:55 +0700

    contoh boolean

--- pertemuan_1/src/main/kotlin/n.bolean.kt
+++ pertemuan_1/src/main/kotlin/n.bolean.kt
@@ -1,6 +1,6 @@
 //Boolean (true or false)
 //Tipe data Boolean memiliki nilai true atau false. Nilai 0 dan 1 tidak berlaku untuk menunjuk
-//nilai pada sebuah variabel bertipe Boolean.
+//nilai pada sebuah variabel bertipe Boolean.,
 fun main(args: Array<String>) {
     var varBool : Boolean = true
     if(varBool)
