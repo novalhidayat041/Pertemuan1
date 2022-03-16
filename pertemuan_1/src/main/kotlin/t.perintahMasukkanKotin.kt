4feadd165a727625d741bdc976eedce003302e3a
Noval <mnovalhidayat298@gmail.com>
2022-03-16 22:42:56 +0700

    contoh perintah masukan kotlin

---
+++ pertemuan_1/src/main/kotlin/t.perintahMasukkanKotin.kt
@@ -0,0 +1,13 @@
+//Perintah Masukan di Kotlin
+//Perintah masukkan di Kotlin yaitu sebuah fungsi readLine()!!. Secara default perintah masukkan
+//di Kotlin akan mengembalikan nilai String ke dalam variabel penampung.
+fun main(args: Array<String>) {
+    //Parrot program
+    println("Hi I'm Lora, the virtual parrot, and i love to repeat!");
+    println("Type something in: ");
+    var input: String
+    input = readLine()!!
+    var output: String
+    output = input + ", " + input + "!"
+    println(output)
+}
